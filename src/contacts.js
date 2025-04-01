d2654e945b888ab6e9dca71358776c1a01719e6a
Yevhenii Pozniak <jekamanu@gmail.com>
2025-04-01 20:10:29 +0300

    finished

--- src/contacts.js
+++ src/contacts.js
@@ -1,22 +1,68 @@
-// contacts.js
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
 
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
+//  * Розкоментуй і запиши значення
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactsPath);
 
 async function listContacts() {
   // ...твій код. Повертає масив контактів.
+  const data = await fs.readFile(contactsPath, {
+    encoding: "utf-8",
+  });
+
+  return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+async function addContact(data) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function updateById(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
 }
 
 async function removeContact(contactId) {
   // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 }
 
-async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
-}
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateById,
+  removeContact,
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { program } from "commander";
+const { program } = require("commander");
+const contacts = require("./contacts");
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -15,18 +17,36 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       // ...
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case "get":
       // ... id
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
       break;
 
     case "add":
       // ... name email phone
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case "edit":
+      // ... id, name email phone
+      const updateContact = await contacts.updateById(id, {
+        name,
+        email,
+        phone,
+      });
+      console.log(updateContact);
       break;
 
     case "remove":
       // ... id
+      const deleteContact = await contacts.removeContact(id);
+      console.log(deleteContact);
       break;
 
     default:
@@ -35,3 +55,28 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(options);
+
+// invokeAction({ action: "list" });
+
+// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
+
+// invokeAction({
+//   action: "add",
+//   name: "Gleck",
+//   email: "glocktar.net",
+//   phone: "(362) 251-6732",
+// });
+
+// invokeAction({
+//   action: "edit",
+//   id: "2UQvpeWUAUOSO2BWfrUIu",
+//   name: "Marina Nazina",
+//   email: "marinazi.net",
+//   phone: "(777) 251-6732",
+// });
+
+// invokeAction({ action: "remove", id: "fHQ7O1jimVf5lJ7mN_QjV" });
+
+//todo команди
+// npm start -- -a list
+// node src/index -a list
